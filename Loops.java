912a8c2997d113092882b71709d2005bbc6529c7
25038822 <25038822>
2025-06-08 03:41:21 +0200

    First commit

---
+++ Loops.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package loops;
+
+/**
+ *
+ * @author Rammbuda Zwonaka
+ */
+//import java.util.Scanner;
+public class Loops {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    for(int i=1;i<=10;i++){
+      System.out.println(i);
+    }
+    for(int i=2;i<=10;i+=2){
+      System.out.println(i);
+    }
+    for(int i=1;i<11;i+=2){
+            System.out.println(i);    
+            
+        }
+       
+        
+    }
+  
+ 
+}
+  
+
